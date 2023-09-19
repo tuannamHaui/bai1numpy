ee8958a4454ea4cd4a4df6e36280655a1d4b72fd
Nam2020608122 <nam1772002@gmail.com>
2023-09-19 09:00:20 +0700

    giai he pt

---
+++ giai_he_PT.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+def giai_he_phuong_trinh(A, B):
+    try:
+        A_inv = np.linalg.inv(A)
+        X = np.dot(A_inv, B)
+        return X
+    except np.linalg.LinAlgError as e:
+        if "Singular matrix" in str(e):
+            return []
+        else:
+            raise e
+
+# Nhập số phương trình và số ẩn từ người dùng
+n = int(input("Nhập số phương trình: "))
+m = int(input("Nhập số ẩn: "))
+
+# Khởi tạo ma trận hệ số A và vector b
+A = np.zeros((n, m))
+B = np.zeros(n)
+
+# Nhập giá trị cho ma trận A và vector B
+for i in range(n):
+    print(f"Nhập phương trình thứ {i + 1}:")
+    for j in range(m):
+        A[i][j] = float(input(f"Nhập hệ số a[{i + 1},{j + 1}]: "))
+    B[i] = float(input(f"Nhập b[{i + 1}]: "))
+
+# Gọi hàm để giải hệ phương trình
+X = giai_he_phuong_trinh(A, B)
+
+if len(X) > 0:
+    print("Nghiệm của hệ phương trình:")
+    print(X)
+elif len(X) == 0:
+    print("Hệ phương trình có vô số nghiệm.")
+else:
+    print("Hệ phương trình vô nghiệm.")
